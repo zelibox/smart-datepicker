b9bc62eb10dba1aaa16d218de220d5b02117cfd3
maxim <zeli.box@gmail.com>
2015-03-15 14:36:15 +0300

    demo structure

--- dist/smart-datepicker.js
+++ dist/smart-datepicker.js
@@ -1,1 +1,7 @@
-angular.module('smartDatepicker', []);
+angular.module('smartDatepicker', [])
+    .directive('smartDatepicker', function () {
+        return {
+            restrict: 'E',
+            template: '<div></div>'
+        }
+    });
